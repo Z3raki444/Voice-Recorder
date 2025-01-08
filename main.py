9fec69f6c8e63b300ba963ddcbfcb3b11ba53694
Z3raki444 <praveenkumarm424@gmail.com>
2025-01-08 19:53:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import  sounddevice as sd
-from scipy.io.wavfile import write
-import wavio as wv
+import sounddevice as sd  # Library for recording and playing sound
+from scipy.io.wavfile import write  # Function to save audio as a WAV file
+import wavio as wv  # Alternative library for saving WAV files
 
-freq = 44100
+# Set the sampling frequency (number of samples per second)
+freq = 44100  # Standard for high-quality audio
 
-duration =5
+# Set the duration of the recording in seconds
+duration = 5  # Recording will last for 5 seconds
 
-recording =sd.rec(int(duration*freq),samplerate=freq,channels=2)
+# Start recording audio with the specified duration, frequency, and stereo channels (2 channels)
+recording = sd.rec(int(duration * freq), samplerate=freq, channels=2)
 
+# Wait for the recording to finish
 sd.wait()
 
-write("recording.wav",freq,recording)
+# Save the recording as a WAV file using scipy's write function
+write("recording.wav", freq, recording)
 
-wv.write("recording.wav",recording,freq,sampwidth=2)    
+# Save the recording as a WAV file using the wavio library (alternative method)
+# The parameter 'sampwidth' specifies the sample width in bytes (2 bytes for 16-bit audio)
+wv.write("recording.wav", recording, freq, sampwidth=2)
